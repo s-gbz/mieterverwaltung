f447bf8cbe7205454f5e46ad8c0ddcb0a7b0551f
Sergej Grilborzer <sergej@grilborzer.de>
2020-05-04 23:58:43 +0200

    Add test stubs

---
+++ src/test/java/service/TenantServiceTest.java
@@ -0,0 +1,24 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TenantServiceTest {
+
+    @Test
+    void readTenantsFromJSON() {
+    }
+
+    @Test
+    void writeTenantsToJSON() {
+    }
+
+    @Test
+    void addTenant() {
+    }
+
+    @Test
+    void deleteTenant() {
+    }
+}
